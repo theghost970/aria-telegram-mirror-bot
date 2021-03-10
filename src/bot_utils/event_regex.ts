86b034cd44ff5acdae82606fbabeb7a2456c9b1f
theghost970 <52626961+theghost970@users.noreply.github.com>
2021-03-10 12:08:03 +0400

    Update event_regex.ts
    Change capital letters to small

--- src/bot_utils/event_regex.ts
+++ src/bot_utils/event_regex.ts
@@ -7,7 +7,7 @@ export class EventRegex {
   readonly commandsRegexNoName: regexps.RegExps;
 
   constructor() {
-    var commands = ['^/start', '^/mirrorTar', '^/mirror', '^/mirrorStatus', '^/list', '^/getFolder', '^/cancelMirror', '^/cancelAll', '^/disk'];
+    var commands = ['^/start', '^/mirrortar', '^/mirror', '^/mirrorstatus', '^/list', '^/getfolder', '^/cancelmirror', '^/cancelall', '^/disk'];
     var commandsNoName: string[] = [];
     var commandAfter = ['$', ' (.+)', ' (.+)', '$', ' (.+)', '$', '$', '$', '$'];
 
